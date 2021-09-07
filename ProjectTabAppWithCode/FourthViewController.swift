34e25442940be488ada4dbfb8294db5b4fb3adc8
joseph-GM <jkim.english75@gmail.com>
2021-09-07 14:24:36 +0900

    first commit

---
+++ ProjectTabAppWithCode/FourthViewController.swift
@@ -0,0 +1,31 @@
+//
+//  FourthViewController.swift
+//  ProjectTabAppWithCode
+//
+//  Created by Joseph Kim on 9/7/21.
+//
+
+import UIKit
+
+class FourthViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .green
+        title = "About"
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
